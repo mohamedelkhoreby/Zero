618636bbdfacb3c89592cd6dd71eef919b4b7e10
mohamed Gamal <mohgamal518@gmail.com>
2024-10-20 03:44:58 +0300

    upload project

---
+++ app/src/main/java/moo/jee/zero/IntroActivity.kt
@@ -0,0 +1,21 @@
+package moo.jee.zero
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import moo.jee.zero.databinding.ActivityIntroBinding
+
+
+class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+        startBtn.setOnClickListener{
+                startActivity(Intent(this@IntroActivity,MainActivity::class.java))
+            }
+        }
+    }
+}
